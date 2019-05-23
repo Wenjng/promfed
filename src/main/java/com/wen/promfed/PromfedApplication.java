93c64b58253f24ee7eceed2afe8726b82f846504
Wenjing <he.wenjing99@gmail.com>
2019-05-22 17:30:07 -0700

    Add JUnit Jupiter and Oauth2 security

--- src/main/java/com/wen/promfed/PromfedApplication.java
+++ src/main/java/com/wen/promfed/PromfedApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.*;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @SpringBootApplication
 @ComponentScan
 public class PromfedApplication {
@@ -12,4 +15,18 @@ public class PromfedApplication {
 		SpringApplication.run(PromfedApplication.class, args);
 	}
 
+	@Configuration
+    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+	    @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt();
+        }
+    }
 }
