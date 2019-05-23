6ce11ddf329a2f77144ee9b9ef43af5fde5604d2
Wenjing <he.wenjing99@gmail.com>
2019-05-22 17:30:07 -0700

    Add JUnit Jupiter and Oauth2 security

--- src/test/java/com/wen/promfed/PromfedApplicationTests.java
+++ src/test/java/com/wen/promfed/PromfedApplicationTests.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PromfedApplicationTests {
 
 	@Test
+//    @DisplayName("MyDummyTest-- todos!!!")
 	public void contextLoads() {
+        assertEquals(2,2);
 	}
 
 }
